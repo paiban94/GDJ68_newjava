430aac62b17973ae2320ec49d481938162e274fc
pabian94 <pabian94@naver.com>
2023-07-07 16:04:12 +0900

    0707

---
+++ src/com/iu/study4/s2/Chef.java
@@ -0,0 +1,44 @@
+package com.iu.study4.s2;
+
+public class Chef {
+
+	String name;
+	
+	public void makeRamyun2(Ramyun [] ramyuns) {
+		for(int i=0;i<ramyuns.length;i++) {
+			Ramyun ramyun = new Ramyun();
+			ramyun.title="진라면";
+			ramyun.price=3000;
+			ramyuns[i]=ramyun;
+		}
+	}
+	
+	public Ramyun [] makeRamyun(int count) {
+
+		//Ramyun ramuyn = new Ramyun(); //클래스명 변수명 = new 클래스명
+		//Ramyun ramuyn2 = new Ramyun();
+		//ramyun2 = ramyun;
+		//ramuyn2 = new Ramyun();
+		//int num=2;
+		//num=3;
+		Ramyun [] ramyuns = new Ramyun[count];
+		for(int i=0;i<count;i++) {
+			Ramyun ramyun = new Ramyun();
+			ramyuns[i]=ramyun;
+			
+			ramyun.title="진라면";
+			ramyun.price=3000;
+			System.out.println(ramyuns[i]);
+			System.out.println(ramyun);
+		} 
+		count = 50;
+		
+		
+		return ramyuns;
+	}
+	
+	public void makekimbap() {
+
+	}
+	
+}
